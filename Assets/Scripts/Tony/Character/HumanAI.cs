4a1463847b69362d8c0b3a867cc7668d52a966f7
tonyli99 <tony@pixelcrushers.com>
2020-02-01 13:57:51 -0500

    AI vision

--- Assets/Scripts/Tony/Character/HumanAI.cs
+++ Assets/Scripts/Tony/Character/HumanAI.cs
@@ -6,28 +6,55 @@ public class HumanAI : MonoBehaviour
 {
     private CharacterActor actor;
 
-    public enum State { Idle, Wander, Attack }
+    public float timeBetweenMovementChange = 5f;
+
+    public enum State { Wander, Attack, Dead }
     [Header("Debug")]
-    public State state = State.Idle;
+    public State state = State.Wander;
     private float nextDirectionChange;
     public Vector2 movement;
+
+    private VisionTrigger visionTrigger;
 
     private void Awake()
     {
         actor = GetComponent<CharacterActor>();
+        visionTrigger = GetComponentInChildren<VisionTrigger>();
     }
 
     private void Update()
     {
         switch (state)
         {
-            case State.Idle:
-                actor.movement = movement;
-                if (Time.time > nextDirectionChange)
+            case State.Wander:
+                if (visionTrigger.seesEnemy)
                 {
-                    movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-                    nextDirectionChange = Time.time + 5;
+                    if (Random.value < 0.5f)
+                    {
+                        movement = new Vector2(1, 0);
+                        nextDirectionChange = Time.time + timeBetweenMovementChange;
+                    }
+                    else
+                    {
+                        actor.Attack();
+                    }
                 }
+                else
+                {
+                    actor.movement = movement;
+                    if (Time.time > nextDirectionChange)
+                    {
+                        movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                        nextDirectionChange = Time.time + timeBetweenMovementChange;
+                    }
+                }
+                break;
+            case State.Attack:
+                movement = Vector2.zero;
+                if (actor.state == CharacterActor.State.Idle) state = State.Wander;
+                break;
+            case State.Dead:
+                movement = Vector2.zero;
                 break;
         }
     }
