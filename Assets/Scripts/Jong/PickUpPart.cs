3df4dde178c8a9c302446d1c8ab27532202d2351
turbonabi <turbonabi@hotmail.com>
2020-02-01 12:40:48 -0500

    Drop and Pickup parts
    scripts and test for dropping and picking up

---
+++ Assets/Scripts/Jong/PickUpPart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpPart : MonoBehaviour
+{
+    public Body body;
+
+    public Limb testLim;
+
+    public float pickupDelay;
+
+    IEnumerator routine;
+    private void Start()
+    {
+        routine = PickUP(pickupDelay);
+        StartCoroutine(routine);
+    }
+
+    IEnumerator PickUP(float waitTime)
+    {
+
+        yield return new WaitForSeconds(waitTime);
+
+        body.ReplaceWith(testLim);
+    }
+}
