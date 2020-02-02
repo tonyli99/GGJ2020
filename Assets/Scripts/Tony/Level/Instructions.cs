4c9b9f835131eb1a7d43e4a2537f48ad9de27986
tonyli99 <tony@pixelcrushers.com>
2020-02-02 13:52:48 -0500

    Main menu instructions

---
+++ Assets/Scripts/Tony/Level/Instructions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Instructions : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKeyDown || Input.GetButtonDown("Fire1") || Input.GetButton("Fire1_Player2"))
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
