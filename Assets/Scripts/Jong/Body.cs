9eb15c75104e354f60a1e1ba07d19583d446fa39
tonyli99 <tony@pixelcrushers.com>
2020-02-02 11:52:15 -0500

    Health and body script changes

--- Assets/Scripts/Jong/Body.cs
+++ Assets/Scripts/Jong/Body.cs
@@ -118,7 +118,7 @@ public class Body : MonoBehaviour
         }
     }
 
-    public void TakeDamange(float damange)
+    public void TakeDamage(float damange)
     {
         foreach (Limb part in bodyParts.Values)
         {
@@ -128,6 +128,6 @@ public class Body : MonoBehaviour
 
     public void TakePotion(float life)
     {
-        TakeDamange(-life);
+        TakeDamage(-life);
     }
 }
